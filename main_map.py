04496fa25165c4b6cf6cae006987d6d0a54ffb17
Alex Smirnov <010407@gmail.com>
2023-02-02 20:50:34 +0300

    Second + third task

--- main_map.py
+++ main_map.py
@@ -1,4 +1,3 @@
-import PyQt5
 import sys
 from PyQt5 import uic
 from PyQt5.QtCore import Qt 
@@ -10,12 +9,41 @@ class Mapper(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Yandex Maps API')
-        uic.loadUi('Interface.ui', self)
-        self.getResult.clicked.connect(self.mapIt)
+        uic.loadUi('Maps-API-Ydx\Interface.ui', self)
+        self.scaler = float()
+        self.getResult.clicked.connect(self.decorator)
+
+    def keyPressEvent(self, event: QtGui.QKeyEvent):
+        print(event.key())
+        koef = 0.25
+        if event.key() == Qt.Key_PageUp and self.scaler < 90 - koef:
+            self.scaler += koef
+            print(self.scaler)
+            self.mapIt(getScale=False, getCenter=False)
+        elif event.key() == Qt.Key_PageDown and self.scaler > koef:
+            self.scaler -= koef
+            print(self.scaler)
+            self.mapIt(getScale=False, getCenter=False)
+        elif event.key() == Qt.Key_W:
+            self.sCord += koef
+            self.mapIt(getScale=False, getCenter=False)
+        elif event.key() == Qt.Key_S:
+            self.sCord -= koef
+            self.mapIt(getScale=False, getCenter=False)
+        elif event.key() == Qt.Key_D:
+            self.fCord += koef
+            self.mapIt(getScale=False, getCenter=False)
+        elif event.key() == Qt.Key_A:
+            self.fCord -= koef
+            self.mapIt(getScale=False, getCenter=False)
+
+    def decorator(self):
+        self.mapIt(getScale=True, getCenter=True)
                 
-    def mapIt(self, getScale=True):
-        self.fCord, self.sCord = float(self.firstCoord.text()), float(self.secondCoord.text())
-        if getScale:
+    def mapIt(self, getScale=True, getCenter=True):
+        if getCenter or not self.sCord:
+            self.fCord, self.sCord = float(self.firstCoord.text()), float(self.secondCoord.text())
+        if getScale or not self.scaler:
             self.scaler = float(self.scale.text())
         get_image((self.fCord, self.sCord), (self.scaler, self.scaler))
         map = QtGui.QPixmap('map.png')
